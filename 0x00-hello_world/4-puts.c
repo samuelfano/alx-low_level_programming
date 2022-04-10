59b73e02b41bdd11d6cdbb579b696a6f0e22eb11
samuel worku <sambforl@gmail.com>
2022-04-10 12:22:24 -0700

    Mycommit message

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -5,8 +5,7 @@
  *Return: 0 after printing the function
  */
 int main(void)
-[
 {
-puts{"\"Programming is like building a multilingual puzzle"};
+puts("\"programming is like building a multilingual puzzle");
 return (0);
 }
